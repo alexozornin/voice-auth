77d4fd7160f376fa2d2f02d95afc7b2bfede0c77
Alexander Ozornin <alexozornin97@gmail.com>
2019-01-19 01:11:55 +0500

    v 0.1.1b updated algo

--- lib/python/modules/audio.py
+++ lib/python/modules/audio.py
@@ -5,24 +5,46 @@ import numpy as np
 import soundfile as sf
 
 
-def getFrequencies(data, samplerate):
+def getFrequencies(data, samplerate, activation = 10):
     fourierList = np.fft.rfft(data)
     fourierLen = len(fourierList)
     frequencies = []
     amplitudes = []
     maxAmp = 0
     maxFreq = 0
+    localMaxFreqs = []
+    localMaxFreq = 300.0
+    localMinAmp = 1
+    localMaxAmp = 1
+    increaseStep = abs(fourierList[0]) < activation
     i = 150  # Min = 300 Hz
     while i < fourierLen and i < 1500:  # Max = 3000 Hz
         frequency = i * samplerate / fourierLen / 2
         amplitude = abs(fourierList[i])
-        if amplitude > maxAmp:
-            maxAmp = amplitude
-            maxFreq = frequency
+        if increaseStep:
+            if amplitude > localMaxAmp:
+                localMaxAmp = amplitude
+                localMaxFreq = frequency
+            else:
+                localMinAmp = amplitude
+                if amplitude < localMaxAmp / 10:
+                    if localMaxAmp > activation:
+                        localMaxFreqs.append(localMaxFreq)
+                    increaseStep = False
+        else:
+            if amplitude < localMinAmp:
+                localMinAmp = amplitude
+            else:
+                localMaxAmp = amplitude
+                if amplitude > localMinAmp * 10:
+                    increaseStep = True
         frequencies.append(frequency)
         amplitudes.append(amplitude)
         i += 1
-    return(frequencies, amplitudes, maxFreq, maxAmp)
+    # print(localMaxFreqs)
+    # plt.plot(frequencies, amplitudes)
+    # plt.show()
+    return(frequencies, amplitudes, localMaxFreqs, maxFreq, maxAmp)
 
 
 def getMaxFrequency(data, samplerate):
@@ -115,11 +137,12 @@ def getLeftAudio(filename):
     data = left
     return(data, datalen, samplerate)
 
-def getAudioDigest(filename):
+def getAudioDigest(filename, activation):
     data, datalen, samplerate = getLeftAudio(filename)
     data = cropAudio(data, samplerate)
     datalen = len(data)
-    if(datalen < samplerate):
+    if(datalen < samplerate / 2):
+        print("Part too short")
         return None
     frameSize = math.floor(samplerate / 2 - 1)
     frameInterval = math.floor((datalen - frameSize) / 15) # for 16 frames
@@ -129,13 +152,11 @@ def getAudioDigest(filename):
     i = 0
     while i < 16:
         datapart = data[startPosition:endPosition]
-        # print('pos', startPosition, endPosition)
-        # print('partl', len(datapart))
-        frequencies, amplitudes, maxFreq, maxAmp = getFrequencies(datapart, samplerate)
+        frequencies, amplitudes, localMaxFreqs, maxFreq, maxAmp = getFrequencies(datapart, samplerate, activation)
         startPosition += frameInterval
         endPosition += frameInterval
         i += 1
-        digest.append(amplitudes)
+        digest.append(localMaxFreqs)
     return digest
 
 def getRMS(digest1, digest2):
@@ -143,27 +164,40 @@ def getRMS(digest1, digest2):
     count = 0
     i = 0
     while i < len(digest1):
+        # plt.plot(digest1[i], 'b', digest2[i], 'r')
+        # plt.show()
+        # print(digest1[i], digest2[i])
         j = 0
         while j < len(digest1[i]):
-            if(digest1[i][j] > 5 or digest2[i][j] > 5):
-                diff = digest1[i][j] - digest2[i][j]
-                rms += math.pow(diff, 2)
-                count += 1
+            if digest1[i][j] < 350:
+                j += 1
+                continue
+            k = 0
+            freq = 300.0
+            minDiff = 80.0
+            while k < len(digest2[i]):
+                diff = abs(digest1[i][j] - digest2[i][k])
+                if diff < minDiff:
+                    minDiff = diff
+                k += 1
+            rms += math.pow(minDiff, 2)
+            count += len(digest1[i])
             j += 1
         i += 1
     rms /= count
+    print(rms)
     return rms
 
 def compare(obj):
-    digestA = getAudioDigest(obj['a'])
+    digestA = getAudioDigest(obj['a'], 5)
     if digestA == None:
         return None
     best = None
-    minRMS = 1.0
+    minRMS = 8192
     tolerance = obj['t']
     i = 0
     while i < len(obj['l']):
-        digestL = getAudioDigest(obj['l'][i])
+        digestL = getAudioDigest(obj['l'][i], 10)
         if digestL == None:
             i += 1
             continue
@@ -172,7 +206,7 @@ def compare(obj):
             best = obj['l'][i]
             minRMS = rms
         i += 1
-    return { 'best': best, 'acc': 1.0 - rms }
+    return { 'best': best, 'rms': minRMS }
 
 while True:
     lst = input()
